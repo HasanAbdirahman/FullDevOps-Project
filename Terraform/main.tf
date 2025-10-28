845474f3ce2f7c7d5e379ffd9795ed5b5b9b2891
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2025-10-28 15:10:27 -0400

    Update main.tf => changed the eks module

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,40 +1,45 @@
-
-# VPC Module
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "4.0.2"
+  version = "~> 5.0"
 
-  name = "eks-vpc"
+  name = "devops-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = {
+    Name        = "devops-vpc"
+    Environment = "dev"
+  }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.4.0"  # Updated version
+  version = "~> 21.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  subnets         = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
+  name               = var.cluster_name
+  kubernetes_version = var.cluster_version
 
-  node_groups = {
-    default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_type    = "t3.medium"
-    }
+  # Use outputs from the VPC module
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
   }
 
   tags = {
     Environment = "dev"
-    Project     = "DevOps-Project"
+    Terraform   = "true"
   }
 }
